02285c2cfc4846859856e863b65803f6cb9be784
Paawan Saxena <jec.paawansaxena@gmail.com>
2024-12-20 14:40:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treats all js code as newer version
+
+// alert(3+3) we are using nodedjs, not browser
+
+console.log(3 +
+    3
+) // code readability should be high
+
+let name = "paawan"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 58
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone CSSMathValue
+// undefines =>
+// symbo => unique
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
