8246dcf9075232772665ae69c742333464878e5f
Paawan Saxena <jec.paawansaxena@gmail.com>
2024-12-20 14:40:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "jec.paawansaxena"
+var acountPassword = "12345"
+accountCity = "jaipur"
+let accountState
+
+// accountId = 2 not allowed
+
+accountEmail ="hc@hc.com"
+accountPassword = "212121"
+accountCity = "banglore"
+
+console.log(accountId)
+
+// prefer not to use var because of issue in 
+// block scope and functional scope
+
+console.table([accountId, accountEmail, accountPassword, accountId, accountCity, accountState])
