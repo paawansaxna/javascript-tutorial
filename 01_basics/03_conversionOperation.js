740b428af6583ef68df457f6fd2a720141b7ad16
Paawan Saxena <jec.paawansaxena@gmail.com>
2024-12-22 05:27:40 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "hitesh"
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1;
+// false => 0
+
+let isloggedIn = "hitesh"
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+
+console.log(booleanIsloggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
